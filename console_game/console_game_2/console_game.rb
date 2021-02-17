2c2255bd9e915d1ba18bee52966558d378a596a3
Mariana Mattos <marianasmmattos@gmail.com>
2021-02-17 17:51:41 -0300

    console game 2 v2

--- console_game/console_game_2/console_game.rb
+++ console_game/console_game_2/console_game.rb
@@ -12,3 +12,60 @@ def escolhe_palavra_secreta
     puts "Palavra escolhida com #{palavra_secreta.size} caracteres... Boa sorte"
     palavra_secreta
 end
+
+def quer_jogar?
+    puts "Deseja jogar novamente? (S/N)"
+    quero_jogar = gets.strip
+    quero_jogar.upcase == "N"
+end
+
+def pede_um_chute(erros, chutes)
+    puts "\n\n\n"
+    puts "Erros até agora: #{erros}"
+    puts "Chutes até agora: #{chutes}"
+    puts "Entre com uma letra ou uma palavra"
+    chute = gets.strip
+    puts "Será que você acertou? Você chutou #{chute}"
+    chute
+end
+
+def joga(nome)
+    palavra_secreta = escolhe_palavra_secreta
+
+    erros = 0
+    chutes = []
+    pontos = 0
+
+    while erros < 5
+        chute = pede_um_chute
+        chutes << chute
+
+        letra = chute.size == 1
+
+        if letra
+
+        else
+            acertou = chute == palavra_secreta
+            if acertou
+                puts "Parabéns! Acertou!"
+                pontos += 100
+                break
+            else 
+                puts "Que pena... Você errou"
+                pontos -= 20
+                erros += 1
+        end
+
+    end
+
+    puts "Você ganhou #{pontos} pontos"
+end
+
+loop do
+    joga nome
+    if quer_jogar?
+        break
+    end
+end
+
+nome = welcome
